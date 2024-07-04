5c82d8ded39680d14caf81a86f0f8f86f5ff99a0
Maria-Liaqat-cmd <marialiaqat68@gmail.com>
2024-07-04 15:41:30 +0500

    Add files via upload

---
+++ variables/class1/index.ts
@@ -0,0 +1,112 @@
+// // calculate your age based on the current year and your birth year:
+
+// var currentYear:number=2024;
+// var birthYear:number=2000;
+// var calculateAge:number=currentYear-birthYear;
+// console.log("My current Age is" + calculateAge);
+
+// // write a programme that calculates the area of rectangle using
+// // length and width variables:
+
+// var lengthOfRectangle:number=36;
+// var widthOfRectangle:number=33;
+// var calculateArea:number=lengthOfRectangle*widthOfRectangle;
+// console.log("AreaOfRectangle is" + calculateArea);
+
+// // write a programme that calculates the area of a circle:
+
+// var radius:number= 65;
+// var areaOfCircle:number=Math.PI*radius**2;
+// console.log("Area Of cicle is" + areaOfCircle);
+
+// // write a programme that calculates the area of the cube:
+
+// var edge:number=90;
+// var areaOfCube:number=6*edge**2;
+// console.log("Area Of Cube is" + areaOfCube);
+
+
+// // create a programme that converts a temperature from
+// // fahrenheit to celcius and vice versa use variable:
+
+// let temperature = 90;
+// let celsius = (temperature - 32) * 5 / 9;
+// console.log(`The temperature in celsius is ${celsius}`);
+// let fahrenheit = celsius * 9 / 5 + 32;
+// console.log(`The temperature in fahrenheit is ${fahrenheit}`);
+
+//write a programme that calculate the seconds/minutes.
+
+//let seconds: number = 5524;
+//let minutes: number = Math.floor(seconds/60);
+//let remainderOfSeconds: number = seconds % 60;
+//console.log(`${seconds} Seconds = ${minutes} Minuts and ${remainderOfSeconds} Seconds`);
+
+// //write a programme that calculates the percentage:
+
+// var totalMarks:number=550;
+// var obtainMarks:number=450;
+// var percentage:number=obtainMarks/totalMarks*100;
+// console.log("Percentage Of number is" +  percentage + "%");
+
+// / incremrnt and decrement operator:
+
+// Q1
+
+// let a = 2;
+// let b = ++a * 2; 
+// = 3*2
+// // b = 6
+
+// Q2
+
+// let x = 5;
+// let y = x-- + 2;
+// 4 + 2
+// // y= 6
+
+// Q3
+
+// let X = 3;
+// let Y= ++x + x++ + ++x;
+// 4+3+4 = 11
+
+// Q4
+
+// let m = 2;
+// let n = ++m * m++ *--m;
+// 3*2*2 =12
+
+// Q5
+
+// let A =3;
+// let B = 5;
+// let result = ++a + b-- - a++ + --b;
+// 4+4-3+4= 9
+
+// Q6
+
+// let m = 2;
+// let n = 4;
+// let p = m++ + ++n - --m + n--;
+// 2+5-2+5=10
+
+// Q7
+
+
+// let a = 5;
+// let b = 3;
+// let c = 2;
+// let d = 7;
+// let result = ++a *(b-- + c) / --d;
+// 6*2+2/6= 12.333333 
+
+// Q8
+
+// let m = 2;
+// let n = 3;
+// let o = 4;
+// let result = m++ *(--n + m) / (o-- - n);
+// 2*2+2/3-3= 1.6666666667
+
+
