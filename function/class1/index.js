e053579fc05dbe7799839edac02467767498d854
Maria-Liaqat-cmd <marialiaqat68@gmail.com>
2024-07-04 15:44:12 +0500

    Add files via upload

---
+++ function/class1/index.js
@@ -0,0 +1,162 @@
+"use strict";
+// using function calculate your age based on the current year and your birth ye
+// function calculateAge(birthYear:number,currentYear:number){
+//var birthYear:number=2000;
+//var currentYear:number=2023;
+// var result:number=birthYear-currentYear;
+// console.log("result",result);
+// }
+// calculateAge(2000,2023)
+// calculateAge(1997,2015)
+// calculateAge(1992,2020)
+// write a programme using function that calculates the area of rectangle using 
+//length and width variables:
+// function calculateTheArea(length:number,width:number){
+//var length:number=24;
+//var width:number=45;
+// var areaOfReactangle:number= length *width ;
+// console.log("AreaOfReactangle",areaOfReactangle);
+// }
+// calculateTheArea(24,45)
+// calculateTheArea(12,33)
+// calculateTheArea(66,90)
+// write a programme using function that calculates the area of a circle:
+// function calculateTheCircle(radius:number,area:number){
+//var radius:number=20;
+//var area:number=31;
+// var areaOfCircle:number=3.14*radius**2;
+// console.log("areaOfCircle",areaOfCircle);
+// }
+// calculateTheCircle(20,31)
+// calculateTheCircle(45,30)
+// calculateTheCircle(6,11)
+// write a programme using function that calculates the area of the cube:
+// function calculateTheCube(edge:number){
+//var edge:number=12;
+// var cubearea:number=6*edge**2;
+// console.log("cubearea",cubearea );
+// }
+// calculateTheCube(12)
+// calculateTheCube(24)
+// create a programme using function that converts a temperature from
+//fahrenheit to celcius and vice versa use variable:
+// function cToFtemprature(tempF:number){
+//var tempF:number=36;
+// var tempF:number=tempF - 32 * 5/9;
+// console.log("tempF",tempF);
+// }
+// cToFtemprature(36)
+// cToFtemprature(22)
+//using functionconvert a given number of seconds into minutes and seconds
+//using variables:
+// function numOfSeconds(seconds:number){
+//ar seconds:number=222;
+// var minutes:number= Math.floor(seconds/60);
+// var remainderOfSeconds: number = seconds % 60;
+// console.log("remainderOfSeconds",remainderOfSeconds);
+// }
+// numOfSeconds(222)
+// numOfSeconds(19);
+//write a programme using function that calculates the percentage:
+// function calculateThePercentage(obtainMarks:number,totalMarks:number){
+//var obtainMarks:number=555;
+//var totalMarks:number=600;
+// var percentage:number=obtainMarks/totalMarks*100;
+// console.log("percentage",percentage);
+// }
+// calculateThePercentage(555,600)
+// calculateThePercentage(200,250)
+// calculateThePercentage(1044,1100)
+// Using function, :check if the number is: - Even or Odd
+// function givenNumber(num1:number){
+//var num1:number=10;
+// if(num1%2==0){
+// console.log("even");
+// }
+// else if(num1%2==0){
+// console.log("odd");
+// }
+// }
+// givenNumber(10)
+//Using function, check if the number is: - Positive, Negative, or Zero.
+// function checknumber(num2:number){
+//var num2:number=15;
+// if(num2>0){
+// console.log("positive");
+// }
+// else if(num2<0){
+// console.log("nagetive");
+// }
+// else(num2==0)
+// console.log("zero")
+// }
+// checknumber(15)
+//Whether it is divisible by both 2 and 3 or anyone of them or not divisible by both check all the cases 
+//and print statement for each case.using function?
+// function check(num3:number){
+//var num3: number = 14;
+// if(num3 %2==0 ){
+// console.log("this number is divisible by both 2 and 3");
+// }
+// else if(num3 %2==0 ){
+// console.log("this number is divisible by 2 not by 3");
+// }else if(num3 %2!=0 ){
+// console.log("this number is divisible 3 not by 2");
+// }
+// else{
+// console.log("this number is not divisible not by 2 neither 3");
+// }
+// }
+// check(14)
+//Take the user age.If the age is 18 or above: Ask if they have a nationality of "Pakistani".If yes, 
+//print "You are eligible to vote."-If no, print "Please obtain a valid ID to vote."using function?
+// Write a program that takes the age of a person as input and determines whether they are a child (0-12 years), 
+//teenager (13-19 years), adult (20-59 years), or senior citizen (60 years and above).using function?
+// function checkpersonage(age2:number){
+// //var age2:number=22;
+// if (age2 >= 0 && age2 <= 12) {
+// console.log ("Child");
+// } else if (age2 >= 13 && age2 <= 19) {
+// console.log ("Teenager");
+// } else if (age2 >= 20 && age2 <= 59) {
+// console.log ("Adult");
+// } else if (age2 >= 60) {
+// console.log("Senior Citizen");
+// } else {
+// console.log ("Invalid Age"); 
+// }
+// }
+// checkpersonage(22)
+//Write a TypeScript function that checks if a given year is a leap year.
+// // The function should return true if it is a leap year, otherwise false.
+// function checkYear(year:number){
+// let year:number=2017;
+// if(year%4===0&& year %100!==0||year %400===0){
+// console.log("leap year");
+// }else
+// console.log("not a leap year");
+// }
+// checkYear(2017);
+// checkYear(2022);
+// checkYear(2020);
+// function getFullName(firstName:string,lastName:string){
+//let firstName:string="maria";
+//let lastName:string="liaqat";
+// let fullName:string=firstName+lastName;
+// console.log(fullName);
+// }
+// getFullName("maria","liaqat");
+// function checkNumber(num1:number,num2:number){
+//let num1:number=10;
+//let num2:number=20;
+// let sum3:number=num1+num2;
+// sum3=sum3*6;
+// console.log(sum3);
+// }
+// checkNumber(10,20);
+// function check(name:string,name1:string){
+//let name:string="john";
+//let name1:string="how are you";
+//console.log(name,name1);
+//}
+//check("john","how are you");
