dc49ef1ff53c96b248ad25cc60408db3a2e989e6
Maria-Liaqat-cmd <marialiaqat68@gmail.com>
2024-07-04 15:46:51 +0500

    Add files via upload

---
+++ Object/class1/index.js
@@ -0,0 +1,25 @@
+"use strict";
+let person = {
+    name: "ali",
+    age: 23,
+    isEmployed: true,
+};
+console.log(person);
+let updateAge = {
+    person: "adil",
+    age: 20,
+    updatePersonAge: 22,
+};
+console.log(updateAge);
+let car = {
+    company: "honda",
+    model: 123,
+    color: "black",
+};
+console.log(car);
+let cars = {
+    company: "toyota",
+    model1: 1234,
+    color1: "white",
+};
+console.log(cars);
